edc3a4a2bd68993acdc40d70942ad06247985949
Vlad Starodubtsev <v.starodubtsev@ndmsystems.com>
2024-08-29 01:52:51 +0300

    custom title

--- aClick.ino
+++ aClick.ino
@@ -16,6 +16,7 @@
 
 #define DEBUG_ETHERNET_WEBSERVER_PORT Serial
 
+#undef _ETHERNET_WEBSERVER_LOGLEVEL_
 #define _ETHERNET_WEBSERVER_LOGLEVEL_ 3  // Debug Level from 0 to 4
 
 #define SHIFT_CHIP_AMOUNT 2
@@ -42,18 +43,22 @@
 #define INPUT_IPV4_ID "ipv4_inp"
 #define INPUT_IPV4_MASK_ID "ipv4_mask_inp"
 #define INPUT_MAC_ID "mac_inp"
+#define INPUT_TITLE_ID "title_inp"
 
 #define BUTTON_RESET_ID "reset_btn"
 #define POPUP_RESET_CONFIRM_ID "reset_cnfrm"
 #define BUTTON_SAVE_ID "save_btn"
 #define BUTTON_CLEAR_ALL_ID "clear_all_btn"
 
+#define TITLE_MAX_LEN 32
+
 struct Data {
   uint32_t ipv4;
   uint32_t mask;
-  uint32_t gw;
+  uint8_t res[4];
   uint8_t mac[ETH_ADDR_LEN];
-  uint8_t reserved[46];
+  char title[TITLE_MAX_LEN];
+  uint8_t reserved[14];
 };
 Data nvData;
 
@@ -74,10 +79,6 @@ int loopCounter = 0;
 
 const char* swPwrPrefix = "sw";
 const char* swRstPrefix = "rst";
-
-String valIpv4;
-String valIpv4Mask;
-String valMac;
 
 String mac2String(uint8_t m[]) {
   String s;
@@ -104,14 +105,35 @@ int parseMac(const char* str, char sep, uint8_t* mac) {
   return 0;
 }
 
+bool validIpMask(const IPAddress& mask) {
+  const uint32_t m32 = __ntohl(mask);
+
+  if (m32 == 0) return false;
+
+  if (m32 & (~m32 >> 1)) {
+    return false;
+  }
+
+  return true;
+}
+
 void uiBuild() {
   GP.BUILD_BEGIN(GP_DARK, 480);
-  GP.PAGE_TITLE(PROJECT_NAME);
-  GP.TITLE(PROJECT_NAME, "t1");
+
+  String title = PROJECT_NAME;
+  String customTitle(nvData.title);
+
+  if (!customTitle.isEmpty() && customTitle.length() <= TITLE_MAX_LEN) {
+    title += " ";
+    title += customTitle;
+  }
+
+  GP.PAGE_TITLE(title);
+  GP.TITLE(title);
 
   GP.HR();
 
-  GP.NAV_TABS_LINKS("/,/info,/settings,/logs,/upgrade", "Home,Information,Settings,Logs,Upgrade");  //TODO ota
+  GP.NAV_TABS_LINKS("/,/info,/settings,/upgrade", "Home,Information,Settings,Upgrade");
 
   if (ui.uri("/info")) {
     GP.SYSTEM_INFO(FIRMWARE_VERSION);
@@ -120,30 +142,34 @@ void uiBuild() {
     GP.FORM_BEGIN("/settings");
     M_BOX(
       GP.LABEL("IP address");
-      GP.TEXT(INPUT_IPV4_ID, IPAddress(nvData.ipv4).toString(), valIpv4));
+      GP.TEXT(INPUT_IPV4_ID, "", IPAddress(nvData.ipv4).toString()));
     M_BOX(
       GP.LABEL("IP mask");
-      GP.TEXT(INPUT_IPV4_MASK_ID, IPAddress(nvData.mask).toString(), valIpv4Mask));
+      GP.TEXT(INPUT_IPV4_MASK_ID, "", IPAddress(nvData.mask).toString()));
     M_BOX(
       GP.LABEL("MAC address");
-      GP.TEXT(INPUT_MAC_ID, mac2String(nvData.mac), valMac));
-    GP.BUTTON_MINI(BUTTON_RESET_ID, "Reset to defaults");
+      GP.TEXT(INPUT_MAC_ID, "", mac2String(nvData.mac)));
+    M_BOX(
+      GP.LABEL("Custom title");
+      GP.TEXT(INPUT_TITLE_ID, "", String(nvData.title), "", TITLE_MAX_LEN));
+
     GP.SUBMIT("Save to NV");
+    GP.BUTTON_MINI(BUTTON_RESET_ID, "Reset to defaults", "", GP_RED);
     GP.FORM_END();
 
     GP.CONFIRM(POPUP_RESET_CONFIRM_ID, F("Are you sure want to reset?"));
     GP.UPDATE_CLICK(POPUP_RESET_CONFIRM_ID, BUTTON_RESET_ID);
-
+    /*
   } else if (ui.uri("/logs")) {
     GP.TEXT("txt", "");
     GP.BUTTON_MINI("btn", "Send", "txt");
     GP.BREAK();
 
     GP.AREA_LOG(32);
-
+*/
   } else if (ui.uri("/upgrade")) {
     GP.OTA_FIRMWARE("Firmware", GP_GREEN, true);
-    // GP.OTA_FILESYSTEM();
+    // GP.OTA_FILESYSTEM("FS", GP_GREEN, true);
   } else {  //home page
     M_BOX(
       M_BLOCK_TAB(
@@ -151,7 +177,6 @@ void uiBuild() {
         for (uint8_t i = 0; i < INTERFACE_ELEMENTS_COUNT; i++) {
           M_BOX(GP.LABEL(String("power") + (i + 1) + ": "); GP.SWITCH(String(swPwrPrefix) + "/" + i, jeromeOutput[i]););
         });
-
       M_BLOCK_TAB(
         "Reset",
         for (uint8_t i = 0; i < INTERFACE_ELEMENTS_COUNT; i++) {
@@ -198,14 +223,13 @@ void uiAction() {
 
     } else if (ui.click(POPUP_RESET_CONFIRM_ID)) {
       if (ui.getBool()) {
-        memset(&nvData, 0, sizeof(nvData));
-        fData.update();
+        eraseNv();
       }
     } else if (ui.click(BUTTON_CLEAR_ALL_ID)) {
       jeromeSetAll(0);
-
+      /*
     } else if (ui.click("btn")) {
-      ui.log.println(ui.getString("btn"));
+      ui.log.println(ui.getString("btn"));*/
     }
   }
 
@@ -231,7 +255,6 @@ void uiAction() {
 
         if (ip.fromString(val)) {
           nvData.ipv4 = ip;
-          nvData.gw = ip & 0xffffff00 + 1;
           changed = true;
         }
       }
@@ -239,12 +262,16 @@ void uiAction() {
       val = ui.getString(INPUT_IPV4_MASK_ID);
 
       if (!val.isEmpty()) {
-        IPAddress ip;
+        IPAddress mask;
 
-        if (ip.fromString(val)) {
-          // TODO checks
-          nvData.mask = ip;
+        if (mask.fromString(val) && validIpMask(mask)) {
+          Serial.print("Set IP mask: ");
+          Serial.println(mask);
+          nvData.mask = mask;
           changed = true;
+        } else {
+          Serial.print("invalid IP mask: ");
+          Serial.println(val);
         }
       }
 
@@ -261,6 +288,13 @@ void uiAction() {
           Serial.print("Set MAC: ");
           Serial.println(mac2String(nvData.mac));
         }
+      }
+
+      val = ui.getString(INPUT_TITLE_ID);
+
+      if (val.length() <= TITLE_MAX_LEN) {
+        strncpy(nvData.title, val.c_str(), sizeof(nvData.title));
+        changed = true;
       }
 
       if (changed) {
@@ -384,7 +418,7 @@ void onTelnetInput(String str) {
   }
 
   if (str.startsWith("$KE")) {
-    GParser data(str.begin(), ',');
+    GParser data(str.begin(), ','); //TODO replace with StringUtils
     int am = data.split();
 
     if (strcmp(data[0], "$KE") != 0) {
@@ -505,6 +539,11 @@ void setupTelnet() {
   }
 }
 
+void eraseNv() {
+  memset(&nvData, 0, sizeof(nvData));
+  fData.update();
+}
+
 void setupNv() {
   memset(&nvData, 0, sizeof(nvData));
 
@@ -541,10 +580,6 @@ void setupNv() {
     nvData.mask = IPAddress(255, 255, 255, 0);
   }
 
-  if (nvData.gw == 0 || !nvOk) {
-    nvData.gw = IPAddress(192, 168, 1, 1);
-  }
-
   if ((nvData.mac[0] == 0 && nvData.mac[1] == 0
        && nvData.mac[2] == 0 && nvData.mac[3] == 0
        && nvData.mac[4] == 0 && nvData.mac[5] == 0)
@@ -557,12 +592,19 @@ void setupNv() {
     nvData.mac[1] |= 0x02;
   }
 
+  for (int i = 0; i < sizeof(nvData.title); i++) {
+    if (nvData.title[i] == 0)
+      break;
+
+    if (!isPrintable((nvData.title[i]))) {
+      memset(nvData.title, 0, sizeof(nvData.title));
+    }
+  }
+
   Serial.print("NV IPv4 ");
   Serial.print(IPAddress(nvData.ipv4));
   Serial.print("/");
   Serial.println(IPAddress(nvData.mask));
-  Serial.print("NV GW ");
-  Serial.println(IPAddress(nvData.gw));
   Serial.print("NV MAC: ");
   Serial.println(mac2String(nvData.mac));
 }
@@ -578,6 +620,14 @@ void setupWdt() {
   esp_task_wdt_init(&twdt_config);
   esp_task_wdt_add(NULL);  //add current thread to WDT watch
   esp_task_wdt_reset();
+}
+
+void setupUi() {
+  ui.attachBuild(uiBuild);
+  ui.attach(uiAction);
+  ui.start();
+  ui.enableOTA();  // login pass
+  //ui.log.start(64);
 }
 
 void setup() {
@@ -603,25 +653,16 @@ void setup() {
 
   esp_iface_mac_addr_set(nvData.mac, ESP_MAC_ETH);
   ETH.begin();
-  ETH.config(IPAddress(nvData.ipv4), IPAddress(nvData.gw), IPAddress(nvData.mask));
+  ETH.config(IPAddress(nvData.ipv4), IPAddress(nvData.ipv4), IPAddress(nvData.mask));
 
-  WT32_ETH01_waitForConnect();
+  //WT32_ETH01_waitForConnect();
 
   setupWdt();
-
-  // start user interface
-  ui.attachBuild(uiBuild);
-  ui.attach(uiAction);
-  ui.start();
-  ui.enableOTA();  // login pass
-  ui.log.start(64);
-
-  Serial.print(F("HTTP EthernetWebServer is @ IP : "));
-  Serial.println(ETH.localIP());
-
+  setupUi();
   setupTelnet();
 
-  Serial.println("Init done");
+  Serial.print("Init done, IP: ");
+  Serial.println(ETH.localIP());
 }
 
 void loop() {
